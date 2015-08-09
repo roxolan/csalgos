0ab27042e472b329b1c4fbaa5d57b55abad7869b
roxolan <roxolanus@gmail.com>
2015-08-10 02:07:39 +0300

    initiated in Xamarin Studio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csalgos
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Csalgos!");
+        }
+    }
+}
